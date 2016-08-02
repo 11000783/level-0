c2695e85c857c24ead890a22002c8533c30f9397
William Bonitz <wbnhw3@gmail.com>
2016-08-01 18:28:36 -0700

    sfyhjknbyu

---
+++ src/HumanZombie.java
@@ -0,0 +1,18 @@
+import java.nio.channels.ShutdownChannelGroupException;
+
+import javax.swing.JOptionPane;
+
+public class HumanZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Yawn");
+	JOptionPane.showMessageDialog(null,"Get out of bed");
+	JOptionPane.showMessageDialog(null, "Go to the bathroom");
+	JOptionPane.showMessageDialog(null, "Brush Teeth");
+	JOptionPane.showMessageDialog(null, "Brush hair");
+	JOptionPane.showMessageDialog(null, "Change out of pj's to day clothes");
+	JOptionPane.showMessageDialog(null, "Go to the kitchen");
+	JOptionPane.showMessageDialog(null, "Have a glass of  orange juice and coffe while reading the paper");
+	JOptionPane.showMessageDialog(null, "Have some waffles");
+}
+
+}
---
+++ src/Knock.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Knock {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("Knock Knock");
+	JOptionPane.showInputDialog("Ya");
+	JOptionPane.showMessageDialog(null, "i am glad to see you to");
+}
+
+}
