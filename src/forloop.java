665049f7e0a4e5283125d6969319af12b4052ce9
Kaiya Park <league@WTS13.attlocal.net>
2016-10-24 18:28:07 -0700

    kyfr

---
+++ src/forloop.java
@@ -0,0 +1,20 @@
+
+public class forloop {
+public static void main(String[] args) {
+//	for (int i = 0; i <= 100; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 100; i >= 0; i--) {
+//		System.out.println(i);
+//	}
+//for (int i = 100; i >= 2; i--) {
+//	System.out.println(i);
+//	
+//	
+	
+	
+	
+	
+	
+}
+}
