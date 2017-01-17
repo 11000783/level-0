8065d1819e299b03e1875e071993194b9ddbdb46
League <rhymeinparentheses@gmail.com>
2017-01-16 18:25:59 -0800

    Hi

---
+++ src/writeAMethod.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class writeAMethod {
+public static void main(String[] args) {
+//iRock("rachel");
+ int agE=age(12);
+System.out.println(agE);
+
+}
+static void iRock(String rock){
+	System.out.println(rock+" rocks");
+	
+	
+	
+}
+static int age(int Age){
+	int aGe = 2016-Age;
+	return aGe;
+	
+}
+
+}
